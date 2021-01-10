7ebfd0eb55e4dd9b875652fdfc4e0f7ee70da75d
vamsirevada <vamsirevada4444@gmail.com>
2021-01-10 19:40:15 +0530

    update

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://arun1234:arun1234@vanity.yqjmn.mongodb.net/vanity?retryWrites=true&w=majority",
-  jwtSecret: "mysecrettoken",
-  clientURL: "http://localhost:3000",
-};
