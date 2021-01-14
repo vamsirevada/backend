f9a95b4fcdd389cebeebcf8c54120c6357039edb
vamsirevada <vamsirevada4444@gmail.com>
2021-01-15 04:58:40 +0530

    link updation

--- routes/api/password.js
+++ routes/api/password.js
@@ -41,8 +41,8 @@ router.put(
         from: `"Vanity Admin" ${process.env.EMAIL_USER}`,
         to: email,
         subject: "Password Reset Instructions",
-        text: `Please use the following link to reset your password: ${process.env.CLIENT_URL}/login/reset-password/${token}`,
-        html: `<p>Please use the following link to reset your password:</p> <p>${process.env.CLIENT_URL}/login/reset-password/${token}</p>`,
+        text: `Please use the following link to reset your password: ${process.env.CLIENT_URL}/reset-password/${token}`,
+        html: `<p>Please use the following link to reset your password:</p> <p>${process.env.CLIENT_URL}/reset-password/${token}</p>`,
       };
 
       return user.updateOne({ resetPasswordLink: token }, (err, success) => {
