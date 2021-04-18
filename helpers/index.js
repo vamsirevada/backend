034b30fe82dedd3767f4664bf643e90ecc2a824c
vamsirevada <vamsirevada4444@gmail.com>
2021-04-18 09:17:55 +0530

    code cleanup

--- helpers/index.js
+++ helpers/index.js
@@ -4,8 +4,8 @@ exports.sendEmail = (emailData) => {
   const transporter = nodeMailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: `${process.env.EMAIL_USER}`,
+      pass: `${process.env.EMAIL_PASS}`,
     },
   });
   return transporter
