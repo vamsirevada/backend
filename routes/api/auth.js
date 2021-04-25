b29a40eea88914683cd65cbfb8b367a51c256442
vamsirevada <vamsirevada4444@gmail.com>
2021-04-26 03:58:41 +0530

    updating...

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -65,7 +65,7 @@ router.post(
       if (!user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+          .json({ errors: [{ msg: 'User does not exist' }] });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
